e638273327c3738fc3cc7aa3bf704a5e35928811
DonaldoAr <jefe.ceo@gmail.com>
2021-09-06 14:45:05 -0500

    Add a configure for model class Persona

--- persona/admin.py
+++ persona/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from persona.models import Persona
 # Register your models here.
+# Registe the model class that we need to register
+
+admin.site.register(Persona)
--- persona/models.py
+++ persona/models.py
@@ -7,3 +7,7 @@ class Persona(models.Model):
     nombre= models.CharField(max_length=255) # Ind. max de caracteres max_lenght
     apellido= models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+    # Persolanizar el ID
+    def __str__(self):
+        return f'Persona {self.id}: {self.nombre} {self.email}'
+        
